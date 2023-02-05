c8839c92466bf9f15d112a8cace3deec46641196
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-05 12:47:12 +0530

    Create a Contact

---
+++ AddressBook/CreateContacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AddressBook
+{
+	public class CreateContacts
+	{
+        public string First_Name { get; set; }
+        public string Middle_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public long PhoneNo { get; set; }
+        public string Email { get; set; }
+    }
+}
+
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,12 +5,27 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using AddressBook;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Address Book Syatem");
+        Console.WriteLine("\nWelcome to AddressBook System");
+        CreateContacts contacts = new CreateContacts()
+        {
+            First_Name = "Puneeth",
+            Middle_Name = "Kumar",
+            Last_Name = "C M",
+            Address = "Nittuvalli",
+            city = "Davangere",
+            state = "Karnataka",
+            zip = 577004,
+            PhoneNo = 8296524711,
+            Email = "puneethkumarcm8050@gmail.com"
+        };
+        Console.WriteLine(contacts.First_Name + "\n" + contacts.Middle_Name + "\n" + contacts.Last_Name + "\n" + contacts.Address + "\n" + contacts.city + "\n"
+        + contacts.state + "\n" + contacts.zip + "\n" + contacts.PhoneNo + "\n" + contacts.Email);
         Console.ReadLine();
     }
 }
